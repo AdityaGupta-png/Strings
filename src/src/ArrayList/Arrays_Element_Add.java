33734832fc5972d20ba9d9674dea4cddb2e6e5ae
Aditya <adityagupta64837@gmail.com>
2026-08-15 10:43:58 +0530

    Wrapper class

---
+++ src/src/ArrayList/Arrays_Element_Add.java
@@ -0,0 +1,4 @@
+package ArrayList;
+
+public class Arrays_Element_Add {
+}
---
+++ src/src/ArrayList/Wrapper_Class.java
@@ -0,0 +1,19 @@
+package ArrayList;
+
+public class Wrapper_Class {
+    static void main() {
+        int a = 10;
+        Integer a1 = 10;  // here it is the Wrapper class of integer
+
+        Long l1 = 44l;  // here it is a wraper class
+        long l2 = 45;
+
+
+        Integer c1 = 15;
+        Integer c2 = 15;
+        Integer c3 = 157;
+        Integer c4 = 157;
+        System.out.println(c1 == c2);
+        System.out.println(c3 == c4);
+    }
+}
