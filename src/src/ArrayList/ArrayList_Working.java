ae40ae617affd551e13be118420c137737d27020
Aditya <adityagupta64837@gmail.com>
2026-08-15 10:47:32 +0530

    Arraylist Internal working

--- src/src/ArrayList/ArrayList_Working.java
+++ src/src/ArrayList/ArrayList_Working.java
@@ -1,4 +1,29 @@
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class ArrayList_Working {
+    static void main() {
+        ArrayList<Integer> ll = new ArrayList<>();
+
+//        Add the element
+        ll.add(10);
+        ll.add(20);
+        ll.add(30);
+        ll.add(40);
+        ll.add(50);
+        ll.add(2,-6);
+        System.out.println(ll);
+        System.out.println(ll.size());
+
+//        Printing the every element of list
+        for(int i = 0;i<ll.size();i++){
+            System.out.print(ll.get(i) + " ");
+        }
+
+
+
+
+
+    }
 }
